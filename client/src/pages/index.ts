78b2adcc9b2a0e1ebfed6324c431108c49d9c92e
huardiane <huardian3154@gmail.com>
2023-02-22 11:09:42 +0800

    first commit

---
+++ client/src/pages/index.ts
@@ -0,0 +1,21 @@
+import Home from "./home";
+import Agent from "./agent";
+import MyProfile from "./my-profile";
+import PropertyDetails from "./property-details";
+import AllProperties from "./all-properties";
+import CreateProperty from "./create-property";
+import AgentProfile from "./agent-profile";
+import EditProperty from "./edit-property";
+import { Login } from "./login";
+
+export {
+  Home,
+  Login,
+  Agent,
+  MyProfile,
+  PropertyDetails,
+  AgentProfile,
+  CreateProperty,
+  EditProperty,
+  AllProperties,
+};
